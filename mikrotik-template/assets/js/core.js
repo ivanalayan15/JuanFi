db522a2ac6d88cce32e69d09c1ed77f82b654fb7
Ivan Julius Alayan <ivanalayan@gmail.com>
2021-09-25 12:11:43 +0800

    #50 - implement
    Signed-off-by: Ivan Julius Alayan <ivanalayan@gmail.com>

--- mikrotik-template/assets/js/core.js
+++ mikrotik-template/assets/js/core.js
@@ -406,9 +406,3 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name+'=; Max-Age=-99999999;';  
 }
-
-function readablizeBytes(bytes) {
-var s = ['bytes', 'kb', 'MB', 'GB', 'TB', 'PB'];
-var e = Math.floor(Math.log(bytes)/Math.log(1024));
-return (bytes/Math.pow(1024, Math.floor(e))).toFixed(2)+" "+s[e];
-}
